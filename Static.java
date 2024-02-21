7c9dab67b212f4887958bc416d9d02ef3cf9848f
CEMRE SEVAL VURAL <50405470+cemresvural@users.noreply.github.com>
2024-02-21 21:13:24 +0300

    21.02.2024, 21:13

--- Static.java
+++ Static.java
@@ -1,12 +1,54 @@
-
+/*import static java.lang.Math.PI;
 public class Static {
-	
-	static int i=10;
-	
+	static int i;
+	static {
+		i=20;
+	}
 	
 	public static void main(String[] args) {
-		System.out.println(i);
-		
+	System.out.println(i);
+	
+	
+	//System.out.println(Math.PI);
+	System.out.println(PI);
+	
+	
+	
 	}
-
+	
+}*/
+//статическая лаб.-static lab 
+public class Static {
+	
+	public static void main(String[] args) {
+		System.out.println(Math.PI);
+		
+		System.out.println(Math.pow(2,3));
+		System.out.println(Math.sqrt(9));
+		System.out.println(Math.min(6,4));
+		System.out.println(Math.max(6,4));
+		
+		float f=1.6f;
+		System.out.println(Math.ceil(f));
+		System.out.println(Math.floor(f));
+		System.out.println(Math.round(f));
+		System.out.println(Math.random()*10);
+ }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
