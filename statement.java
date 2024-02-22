c17542050d3c6866fe32f957957b3365a49f9977
CEMRE SEVAL VURAL <50405470+cemresvural@users.noreply.github.com>
2024-02-22 23:49:18 +0300

    22.02.2024, 23:49

---
+++ statement.java
@@ -0,0 +1,24 @@
+package statements;
+/*
+public class statement {
+	public static void main(String[] args) {
+		int i;//expression
+		int x=10;//decleration
+		System.out.println("red");
+		double pow=Math.pow(2, 3);
+		System.out.println(pow);
+		
+		for(int j=0;j<10;j++) {
+			System.out.println(j);
+		}
+	}
+}*/
+public class statement {
+	public static void main(String[] args) {
+		int x=10;
+		int y=20;
+		int z=x+y;
+		System.out.println(x+y);
+		System.out.println(z);
+	}
+}
