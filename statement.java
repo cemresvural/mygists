959fef98b7ca7f9f52c53df85d5aa42aad139c4d
CEMRE SEVAL VURAL <50405470+cemresvural@users.noreply.github.com>
2024-02-25 17:50:40 +0300

    25.02.2024, 17:50

--- statement.java
+++ statement.java
@@ -15,10 +15,40 @@ public class statement {
 }*/
 public class statement {
 	public static void main(String[] args) {
-		int x=10;
-		int y=20;
-		int z=x+y;
-		System.out.println(x+y);
-		System.out.println(z);
+//		int x=10;
+//		int y=20;
+//		int z=x+y;
+//		System.out.println(x+y);
+//		System.out.println(z);
+//		
+//		
+//		//Lab1
+//		System.out.println(x-y);//unsigned!
+//		System.out.println(x*y);
+//		System.out.println(x/y);
+//		System.out.println(5%2);
+//		
+//		
+//		
+//		int a=10;
+//		//a=a+2;
+//		a+=2;
+//		System.out.println(a);
+		
+		
+		//Lab2
+		
+		
+		
+		
+		int i=10;//00000000 00000000 00000000 00001010
+		         
+		int x=4;//00000000 00000000 00000000 00000100
+		System.out.println(~i);//11111111 11111111 11111111 11110101
+		System.out.println(i & x);//00000000 00000000 00000000 00000000
+		System.out.println(i | x);
+		System.out.println(x>>1);//When I shift the number to the right once, I actually divide the number by 2.
+		System.out.println(x<<1);//when  I shift the number to the left once,the leftmost bit is discarded and a "0" is added to the end.
+
 	}
 }
