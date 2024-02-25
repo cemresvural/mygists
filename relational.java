8bf57d27e13af6f7e2856a830aed2dc4a9fb2ce4
CEMRE SEVAL VURAL <50405470+cemresvural@users.noreply.github.com>
2024-02-25 21:25:18 +0300

    25.02.2024, 21:25

---
+++ relational.java
@@ -0,0 +1,50 @@
+
+public class relational {
+         public static void main(String[] args) {
+        	 /*
+        	 int x=10;
+        	 int y=20;
+        	 System.out.println(x>y);
+        	 System.out.println(x>=y);
+        	 System.out.println(x<y);
+        	 System.out.println(x<=y);
+        	 System.out.println(x==y);
+        	 System.out.println(x!=y);
+        	 
+        	 boolean b = x ==y;
+        	 System.out.println(b);
+        	 */
+        	 
+        	 //logical
+        	 /*
+        	 boolean b=false;
+        	 System.out.println(b);
+        	 System.out.println(!b);
+        	 System.out.println(b&false);
+        	 //false & false=false
+        	 //false & true =false
+        	 //true & false =false
+        	 //true & true = true
+        	 
+        	//false | false=false
+        	 //false | true =true
+        	 //true | false =true
+        	 //true | true = true
+        	 
+        	 System.out.println(b |false);
+        	 System.out.println(b&&false);
+        	 
+        	 
+        	 
+      */  	 
+        	 //Lab
+        	 
+        	 boolean sonuc= 2>5;
+        	 System.out.println(sonuc);
+        	 
+        	 //<,>,==,<=,>=,!=
+        	 
+        	 
+        	 
+         }
+}
---
+++ ternaryIf.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class ternaryIf {
+	 public static void main(String[] args) {
+		 Scanner scanner =new Scanner(System.in);
+		 System.out.print("Yasınız: ");
+		 int age =scanner.nextInt();
+		 System.out.println(age >= 18 ? "Ehliyet alabilir.": "Ehliyet alamaz.");
+		 //The operation requires 3 operands 
+		 
+		 /*Operatör öncelikleri
+		  1-parantez kullanımı
+		  2-unary operatörler
+		  3-carpma,bolme ve mod operatörleri
+		  4-toplama ve cıkarma op.
+		  5-ilişkisel op.
+		  6-eşit ve eşit değildir op.
+		  7-mantıksal veya operatörü
+		  */
+	 }
+}
