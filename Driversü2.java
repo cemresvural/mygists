21b9ebfcc8e7708e5875bac3084292be1ac8da49
CEMRE SEVAL VURAL <50405470+cemresvural@users.noreply.github.com>
2024-03-09 21:42:22 +0300

    09.03.2024, 21:42

---
+++ Driversü2.java
@@ -0,0 +1,41 @@
+
+public class Driversü2 {
+	public static void main(String[] args) {
+		Oyuncu oyuncu1 =new Oyuncu();
+		Oyuncu oyuncu2=new Oyuncu();
+		Oyuncu oyuncu3=new Oyuncu();
+		
+		oyuncu3.oyuncuSayisi();
+		/*System.out.println(oyuncu1.saglik);
+		for(int i=0; i<30; i++) {
+			oyuncu1.darbe();
+			System.out.println(oyuncu1.saglik);
+		}
+		oyuncu1.yasam();
+		System.out.println(oyuncu1.saglik);
+		*/
+		
+	}
+
+}
+//Oyuncu.java
+
+//public class Oyuncu {
+//int saglik=100;
+//static int oyuncuSayisi=0;
+//
+//
+//public Oyuncu() {
+//	oyuncuSayisi++;
+//}
+//void yasam() {
+//	saglik=100;
+//}
+//
+//void darbe() {
+//	saglik--;
+//}
+//void oyuncuSayisi() {
+//	System.out.println(oyuncuSayisi);
+//}
+//}
