9de5cbd7f2d7f3dded248dba14b861c484bf13c1
CEMRE SEVAL VURAL <50405470+cemresvural@users.noreply.github.com>
2024-04-09 23:57:31 +0300

    09.04.2024,

--- rentACar/src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
+++ rentACar/src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
@@ -7,20 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kodlama.io.rentACar.business.abstracts.BrandService;
+import kodlama.io.rentACar.business.requests.CreateBrandRequest;
 import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
+import kodlama.io.rentACar.core.utilities.mappers.ModelMapperService;
 import kodlama.io.rentACar.dataAccess.abstracts.BrandRepository;
 import kodlama.io.rentACar.entities.concretes.Brand;
+import lombok.AllArgsConstructor;
 
 @Service
+@AllArgsConstructor
 public class BrandManager implements BrandService{
 	private BrandRepository brandRepository;
-    
-	@Autowired
-	public BrandManager(BrandRepository brandRepository) {
-		super();
-		this.brandRepository = brandRepository;
-	}
-
+    private ModelMapperService modelMapperService;
 
 	@Override
 	public List<GetAllBrandsResponse> getAll() {
@@ -36,6 +34,15 @@ public class BrandManager implements BrandService{
 		}
 		
 		return  brandsResponse;
+	}
+
+
+	@Override
+	public void add(CreateBrandRequest createBrandRequest) {
+		
+		Brand brand=this.modelMapperService.forRequest().map(createBrandRequest, Brand.class);
+		this.brandRepository.save(brand);
+		
 	}
 
 }
