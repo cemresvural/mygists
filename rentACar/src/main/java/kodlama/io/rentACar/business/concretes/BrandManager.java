9c71e71e11d923757c39437e4688f5d453bbe7b5
CEMRE SEVAL VURAL <50405470+cemresvural@users.noreply.github.com>
2024-04-11 22:06:17 +0300

    11.04.2024,Четверг,22:06

--- rentACar/src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
+++ rentACar/src/main/java/kodlama/io/rentACar/business/concretes/BrandManager.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Service;
 
 import kodlama.io.rentACar.business.abstracts.BrandService;
 import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.requests.UpdateBrandRequest;
 import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
+import kodlama.io.rentACar.business.responses.GetByIdBrandsResponse;
 import kodlama.io.rentACar.core.utilities.mappers.ModelMapperService;
 import kodlama.io.rentACar.dataAccess.abstracts.BrandRepository;
 import kodlama.io.rentACar.entities.concretes.Brand;
@@ -37,6 +39,30 @@ public class BrandManager implements BrandService{
 		
 		Brand brand=this.modelMapperService.forRequest().map(createBrandRequest, Brand.class);
 		this.brandRepository.save(brand);
+		
+	}
+
+
+	@Override
+	public GetByIdBrandsResponse getById(int id) {
+		Brand brand =this.brandRepository.findById(id).orElseThrow();
+		
+		GetByIdBrandsResponse response=this.modelMapperService.forResponse().map(brand, GetByIdBrandsResponse.class);
+		return response;
+	}
+
+
+	@Override
+	public void update(UpdateBrandRequest updateBrandRequest) {
+		Brand brand=this.modelMapperService.forRequest().map(updateBrandRequest, Brand.class);
+		this.brandRepository.save(brand);
+		
+	}
+
+
+	@Override
+	public void delete(int id) {
+		this.brandRepository.deleteById(id);
 		
 	}
 
--- rentACar/src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
+++ rentACar/src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
@@ -3,8 +3,11 @@ package kodlama.io.rentACar.webApi.controllers;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -12,7 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import kodlama.io.rentACar.business.abstracts.BrandService;
 import kodlama.io.rentACar.business.requests.CreateBrandRequest;
+import kodlama.io.rentACar.business.requests.UpdateBrandRequest;
 import kodlama.io.rentACar.business.responses.GetAllBrandsResponse;
+import kodlama.io.rentACar.business.responses.GetByIdBrandsResponse;
 import lombok.AllArgsConstructor;
 
 @RestController //annotation
@@ -26,10 +31,28 @@ public class BrandsController {
 	public List<GetAllBrandsResponse> getAll(){
 		return brandService.getAll();
 		}
+	
+
+	@GetMapping("/{id}")
+	public GetByIdBrandsResponse getById(@PathVariable int id){
+		return brandService.getById(id);
+		}
+	
+	
    @PostMapping()
    @ResponseStatus(code=HttpStatus.CREATED)
    public void add(@RequestBody() CreateBrandRequest  createBrandRequest) {
 	   this.brandService.add(createBrandRequest);
+   }
+   
+   @PutMapping
+   public void update(@RequestBody() UpdateBrandRequest updateBrandRequest) {
+	    this.brandService.update(updateBrandRequest);
+   }
+   
+   @DeleteMapping("/{id}")
+   public void delete(@PathVariable int id) {
+	   this.brandService.delete(id);
    }
 	
 }
